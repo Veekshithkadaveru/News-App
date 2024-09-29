70d57ee65c38f661a5978e632b5f91870256b0eb
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-09-29 20:49:49 +0530

    Add App Constants and Language Helper

---
+++ app/src/main/java/com/example/newsapp/utils/AppConstants.kt
@@ -0,0 +1,17 @@
+package com.example.newsapp.utils
+
+object AppConstants {
+    const val API_KEY = "1e801e018a254095abd5a8d88026cad4"
+
+    const val DEFAULT_COUNTRY_CODE = "IN"
+
+    const val DEFAULT_COUNTRY_NAME = "India"
+
+    const val DEFAULT_COUNTRY_FLAG = "🇮🇳"
+
+    const val DEFAULT_LANGUAGE_CODE = "en"
+
+    const val DEFAULT_SOURCE = "All"
+
+    const val PAGE_SIZE = 15
+}
