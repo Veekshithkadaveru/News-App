cf46e38a97d7c1f0c559f93fe86e01409997aeb9
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-01 16:12:24 +0530

    Setup DI

--- app/src/main/java/com/example/newsapp/di/Qualifiers.kt
+++ app/src/main/java/com/example/newsapp/di/Qualifiers.kt
@@ -1,2 +1,11 @@
 package com.example.newsapp.di
 
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApiKey
--- app/src/main/java/com/example/newsapp/di/module/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/module/AppModule.kt
@@ -1,13 +1,29 @@
-package com.example.newsapp.di
+package com.example.newsapp.di.module
 
 import android.content.Context
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.room.Room
+import com.example.newsapp.data.local.AppDatabase
+import com.example.newsapp.data.local.AppDatabaseService
+import com.example.newsapp.data.local.DatabaseService
+import com.example.newsapp.data.remote.AuthInterceptor
+import com.example.newsapp.data.remote.model.NetworkService
+import com.example.newsapp.di.ApiKey
+import com.example.newsapp.di.BaseUrl
+import com.example.newsapp.utils.AppConstants.API_KEY
+import com.example.newsapp.utils.DefaultDispatcherProvider
+import com.example.newsapp.utils.DispatcherProvider
 import com.example.newsapp.utils.network.NetworkConnected
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -27,4 +43,61 @@ class AppModule {
     }
 
     @BaseUrl
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String {
+        return "https://newsapi.org/v2/"
+    }
+
+    @ApiKey
+    @Singleton
+    @Provides
+    fun provideApiKey(): String {
+        return API_KEY
+    }
+
+    @Singleton
+    @Provides
+    fun provideNetworkService(@BaseUrl baseUrl: String, @ApiKey apiKey: String): NetworkService {
+        val authInterceptor = AuthInterceptor(apiKey = apiKey)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+
+        val contentType = "application/json".toMediaType()
+
+        val json = Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+            .create(NetworkService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = AppDatabase::class.java,
+            name = "News App"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesDatabaseService(appDatabase: AppDatabase): DatabaseService {
+        return AppDatabaseService(appDatabase)
+    }
+
+    @Singleton
+    @Provides
+    fun provideDispatchProvider(): DispatcherProvider = DefaultDispatcherProvider()
+
 }
