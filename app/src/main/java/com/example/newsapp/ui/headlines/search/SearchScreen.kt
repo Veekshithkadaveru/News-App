02a74d27b006118905f3a0f3416c9c17a8c4a14b
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-16 23:08:14 +0530

    Create Search Field

--- app/src/main/java/com/example/newsapp/ui/headlines/search/SearchScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/headlines/search/SearchScreen.kt
@@ -2,25 +2,34 @@ package com.example.newsapp.ui.headlines.search
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.flow.debounce
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -33,6 +42,9 @@ import com.example.newsapp.ui.base.Route
 import com.example.newsapp.ui.base.SearchHandler
 import com.example.newsapp.ui.headlines.LoadPaginatedHeadlines
 import com.example.newsapp.utils.StringsHelper.StringsHelper.SAVED_TO_BOOKMARK
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -102,7 +114,7 @@ fun TopAppBar(onBackPressed: ClickHandler, onQueryChange: SearchHandler) {
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 BackButton { onBackPressed() }
-                SearchField(onQueryChange(it))
+                SearchField(onQueryChange)
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
@@ -112,11 +124,47 @@ fun TopAppBar(onBackPressed: ClickHandler, onQueryChange: SearchHandler) {
     )
 }
 
+
 @Composable
-fun SearchField(onQueryChange: SearchHandler) {
+private fun SearchField(onQueryChange: SearchHandler) {
 
-    TODO("Create Search Field")
+    var searchText by remember { mutableStateOf("") }
 
+    LaunchedEffect(searchText) {
+        MutableStateFlow(searchText)
+            .debounce(300)
+            .filter { it.isNotBlank() }
+            .distinctUntilChanged()
+            .collect { onQueryChange(it) }
+    }
 
-
+    BasicTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 24.dp),
+        value = searchText,
+        onValueChange = { query -> searchText = query },
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colorScheme.primary),
+        decorationBox = { innerTextField ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(MaterialTheme.colorScheme.onPrimaryContainer)
+                    .padding(12.dp),
+            ) {
+                if (searchText.isEmpty()) {
+                    Text(
+                        text = "search...",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
 }
