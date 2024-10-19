75f844aa9f7d76806a91fb0e9fb7aa2768c4a65f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-19 22:29:29 +0530

    Add Scaffold with TopAppBar

--- app/src/main/java/com/example/newsapp/ui/headlines/bookmarks/BookmarksScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/headlines/bookmarks/BookmarksScreen.kt
@@ -1,11 +1,14 @@
 package com.example.newsapp.ui.headlines.bookmarks
 
 import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -13,13 +16,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.newsapp.R
 import com.example.newsapp.ui.base.BackButton
 import com.example.newsapp.ui.base.ClickHandler
 import com.example.newsapp.ui.base.UrlHandler
+import com.example.newsapp.ui.headlines.LoadHeadlines
 import com.example.newsapp.utils.StringsHelper.StringsHelper.BOOKMARKED_NEWS
+import com.example.newsapp.utils.StringsHelper.StringsHelper.REMOVED_FROM_BOOKMARKS
 
 @Composable
 fun BookmarksScreen(
@@ -31,7 +38,27 @@ fun BookmarksScreen(
 
     val savedHeadlinesState by bookmarksViewmodel.headlineList.collectAsState()
 
-    TODO("Added Scaffold with TopAppBar")
+    Scaffold(topBar = {
+        TopAppBar { navController.navigateUp() }
+    }) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                LoadHeadlines(
+                    headlinesState = savedHeadlinesState,
+                    isNetworkConnected = true,
+                    bookmarkIcon = painterResource(id = R.drawable.delete),
+                    onHeadlineClicked = { onHeadlineClicked(it.url) },
+                    onBookmarkClicked = {
+                        bookmarksViewmodel.removeFromBookmarkedHeadlines(it)
+                        Toast.makeText(context, REMOVED_FROM_BOOKMARKS, Toast.LENGTH_SHORT).show()
+                    },
+                    onRetryClicked = { })
+
+            }
+
+        }
+
+    }
 
 }
 
