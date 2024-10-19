d9696c74588dcc9dbca45487963e7f124e5fb795
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-19 12:51:18 +0530

    Create Add bookmarking functionality and toast notification in Scaffold layout

--- app/src/main/java/com/example/newsapp/ui/headlines/offline/OfflineScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/headlines/offline/OfflineScreen.kt
@@ -1,11 +1,15 @@
 package com.example.newsapp.ui.headlines.offline
 
 import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -13,12 +17,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.newsapp.R
 import com.example.newsapp.ui.base.BackButton
 import com.example.newsapp.ui.base.ClickHandler
+import com.example.newsapp.ui.headlines.LoadHeadlines
 import com.example.newsapp.utils.StringsHelper.StringsHelper.OFFLINE_NEWS
+import com.example.newsapp.utils.StringsHelper.StringsHelper.SAVED_TO_BOOKMARK
+
 
 @Composable
 fun OfflineScreen(
@@ -30,7 +39,26 @@ fun OfflineScreen(
 
     val headlinesState by viewmodel.headlineList.collectAsState()
 
-    TODO("Create Add bookmarking functionality and toast notification in Scaffold layout")
+    Scaffold(topBar = { TopAppBar { navController.navigateUp() } })
+    { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Box(modifier = Modifier.fillMaxWidth()) {
+                LoadHeadlines(
+                    headlinesState = headlinesState,
+                    isNetworkConnected = true,
+                    bookmarkIcon = painterResource(id = R.drawable.add),
+                    onHeadlineClicked = { onHeadlineClicked(it.url) },
+                    onBookmarkClicked = {
+                        viewmodel.bookmarkHeadline(it)
+                        Toast.makeText(context, SAVED_TO_BOOKMARK, Toast.LENGTH_LONG).show()
+                    },
+                    onRetryClicked = { })
+
+            }
+
+        }
+
+    }
 
 }
 
