44b8e7a06f1eddd6e94ef86ca548dd8eb73838c4
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-10-27 21:56:19 +0530

    Update Mainactivity

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -3,28 +3,41 @@ package com.example.newsapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.newsapp.ui.base.AppNavHost
 import com.example.newsapp.ui.theme.NewsAppTheme
+import com.example.newsapp.utils.network.NetworkConnected
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var networkConnected: NetworkConnected
+
+    @Inject
+    lateinit var customTabsIntent: CustomTabsIntent
+
+    private val mainViewmodel: MainViewmodel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        observeNetworkChanges()
         setContent {
             NewsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                AppNavHost(mainViewModel = mainViewmodel, customTabsIntent = customTabsIntent)
             }
+        }
+    }
+
+    private fun observeNetworkChanges() {
+        networkConnected.observe(this@MainActivity) {
+            mainViewmodel.updateNetworkStatus(it)
         }
     }
 }
